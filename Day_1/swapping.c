febd852e6881d6dfb6b2d5c1cc923bdc239ff3d4
SaffrynTimothy <111496915+SaffrynTimothy@users.noreply.github.com>
2023-12-21 11:33:56 +0530

    Create swapping.c

---
+++ Day_1/swapping.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    printf("num1: ");
+    scanf("%d", &num1);
+    printf("num2: ");
+    scanf("%d", &num2);
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    printf("After swapping:\n");
+    printf("num1 = %d\n", num1);
+    printf("num2 = %d\n", num2);
+    return 0;
+}
